1422596a2209f08e23ccb1c94b0b7a66cfe16f78
atalayatasoy <61522977+atalayatasoy@users.noreply.github.com>
2020-06-30 17:13:17 +0000

    Update dataProcess.js

--- dataProcess.js
+++ dataProcess.js
@@ -11,11 +11,11 @@ let noTestingInfo = info.data.filter(e => e[e.length - 1] === null).length;
 console.log(noTestingInfo); 
 
  // overall top 10
-let overallScoresTop10 = info.data.sort((a, b) => (b[13] + b[12] + b[11]) - (a[13] + a[12] + a[11])).filter((e, i) => i < 9).map(e => e[9]);
+let overallScoresTop10 = info.data.sort((a, b) => (b[13] + b[12] + b[11]) - (a[13] + a[12] + a[11])).filter((e ,i) => i < 10).map(e => e[9]);
 console.log(overallScoresTop10)
 
  //divergent top 10
-let divergencyMathReading = info.data.sort((a ,b) => Math.abs(b[12] - b[11]) - Math.abs(a[12] -a[11])).filter((e, i) => i < 9).map(e => e[9])
+let divergencyMathReading = info.data.sort((a ,b) => Math.abs(b[12] - b[11]) - Math.abs(a[12] -a[11])).filter((e, i) => i < 10).map(e => e[9])
 console.log(divergencyMathReading)
 
 // my question: average of total test takers in all schools data
